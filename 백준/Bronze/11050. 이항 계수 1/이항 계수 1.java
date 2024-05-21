ae5411e4f827e948fbe8369f13e8ffc338a995ce
Kong <70522355+hyeok-kong@users.noreply.github.com>
2024-05-21 15:34:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[][] D = new int[N+1][N+1];
+        for (int i = 0; i <= N; i++) {
+            D[i][1] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i-1][j] + D[i-1][j-1];
+            }
+        }
+        System.out.println(D[N][K]);
+    }
+}
