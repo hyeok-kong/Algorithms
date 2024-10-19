19f2ebd99feb7e4fac7158f769413069736caebb
Kong <khb6997@gmail.com>
2024-10-20 00:30:16 +0900

    [Gold IV] Title: 최단경로, Time: 1832 ms, Memory: 323900 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,66 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static class Node implements Comparable<Node> {
-        int v;
-        int w;
-        public Node(int v, int w) {
-            this.v = v;
-            this.w = w;
+        int node;
+        int weight;
+
+        public Node(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
         }
 
         @Override
         public int compareTo(Node n) {
-            if (this.w > n.w) return 1;
-            else return -1;
+            return weight > n.weight ? 1 : -1;
         }
     }
 
+    static List<Node>[] graph;
+    static int[] distance;
+    static boolean[] visited;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int V = sc.nextInt();
         int E = sc.nextInt();
+        int K = sc.nextInt();
 
-        ArrayList<Node>[] graph = new ArrayList[V+1];
-        int[] distances = new int[V+1];
-        for (int i=1;i<=V;i++) {
+        visited = new boolean[V+1];
+        distance = new int[V+1];
+        graph = new ArrayList[V+1];
+        for (int i=0; i<=V; i++) {
             graph[i] = new ArrayList<>();
-            distances[i] = Integer.MAX_VALUE;
+            distance[i] = Integer.MAX_VALUE;
         }
-
-        int start = sc.nextInt();
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        boolean[] visited = new boolean[V+1];
-        q.add(new Node(start, 0));
-        distances[start] = 0;
 
         for (int i=0;i<E;i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            int weight = sc.nextInt();
-            graph[from].add(new Node(to, weight));
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+
+            graph[u].add(new Node(v, w));
         }
 
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-            if (visited[now.v]) continue;
-            visited[now.v] = true;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(K, 0));
+        distance[K] = 0;
 
-            for (int i=0;i<graph[now.v].size();i++) {
-                Node temp = graph[now.v].get(i);
-                if (distances[temp.v] > distances[now.v] + temp.w) {
-                    distances[temp.v] = distances[now.v] + temp.w;
-                    q.add(new Node(temp.v, distances[temp.v]));
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (visited[now.node]) {
+                continue;
+            }
+            visited[now.node] = true;
+
+            for (Node node : graph[now.node]) {
+                if (distance[node.node] > distance[now.node] + node.weight) {
+                    distance[node.node] = distance[now.node] + node.weight;
+                    pq.add(new Node(node.node, distance[node.node]));
                 }
             }
         }
 
         for (int i=1;i<=V;i++) {
             if (visited[i]) {
-                System.out.println(distances[i]);
+                System.out.println(distance[i]);
             } else {
                 System.out.println("INF");
             }
