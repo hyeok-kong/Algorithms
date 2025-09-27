183b9a7e0783775edb2773e0e71911ba2e02b4a2
Kong <khb6997@gmail.com>
2025-09-27 14:10:44 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 77.70 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -3,15 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] cards) {
         int answer = 0;
+        Map<Integer, Set<Integer>> groups = new HashMap<>();
         
         for (int i=0;i<cards.length;i++) {
             Set<Integer> group1 = new HashSet<>();
-            dfs(i, cards, group1);
+            if (groups.get(i) != null) {                
+                group1 = groups.get(i);
+            } else {
+                dfs(i, cards, group1);
+                for (int item : group1) {
+                    groups.put(item, group1);
+                }
+            }
             
             for (int j=0;j<cards.length;j++) {
                 Set<Integer> group2 = new HashSet<>(group1);
                 dfs(j, cards, group2);
-                
                 group2.removeAll(group1);
                 answer = Math.max(answer, group1.size() * group2.size());
             }
@@ -28,4 +35,14 @@ class Solution {
         dfs(cards[index]-1, cards, set);
     }
     
+    
+    static void printSet(Set<Integer> set) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for (int item : set) {
+            sb.append(item).append(", ");
+        }
+        sb.append(">");
+        System.out.println(sb.toString());
+    }
 }
